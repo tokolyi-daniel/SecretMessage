44690c0620a00b5085b6e6039d42cac9e0d5ab81
tokolyidaniel <tokolyidaniel@gmail.com>
2025-05-18 15:12:27 +0200

    Implemented SecretCode class with constructor and CreateSecretMessage method to generate encrypted messages.

---
+++ src/SecretMessage/SecretCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretMessage
+{
+    class SecretCode : SecretMessage
+    {
+        public SecretCode(string message, string key) : base(key, "unknown", message)
+        {
+        }
+
+        public string CreateSecreteMessage()
+        {
+            string secretString = string.Empty;
+            for (int i = 0; i < Message.Length; i++)
+            {
+                int x = CharacterConverter(Message[i]) + CharacterConverter(Key[i]);
+                if (x < 26) { secretString += IntConverter(x); }
+                else { secretString += IntConverter(x % 27); }
+            }
+            return secretString;
+        }
+    }
+}
