723794fff9822caa3239dc650fb04ecc4aebec92
tokolyidaniel <tokolyidaniel@gmail.com>
2025-05-18 13:55:08 +0200

    Added initial class with fields, properties (getters/setters), and created a related exception class. Created a method which converts a character to int, based on the predetermined logic and one that converts it back to character.

---
+++ src/SecretMessage/SecretMessage.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretMessage
+{
+    class SecretMessage
+    {
+            protected string key;
+            protected string secretLine;
+            protected string message;
+
+            public SecretMessage(string key, string secretLine, string message)
+            {
+                Key = key;
+                SecretLine = secretLine;
+                Message = message;
+            }
+
+            public string Key
+            {
+                get => key;
+                set
+                {
+                    foreach (char c in value)
+                    {
+                        if (!(c >= 'a' && c <= 'z' || c == ' '))
+                        {
+                            throw new SecretMessage_Exception("");
+                        }
+                    }
+                    key = value.ToLower();
+                }
+            }
+
+            public string SecretLine
+            {
+                get => secretLine;
+                set
+                {
+                    foreach (char c in value)
+                    {
+                        if (!(c >= 'a' && c <= 'z' || c == ' '))
+                        {
+                            throw new SecretMessage_Exception("");
+                        }
+                    }
+                    secretLine = value.ToLower();
+                }
+            }
+
+            public string Message
+            {
+                get => message;
+                set
+                {
+
+                    foreach (char c in value)
+                    {
+                        if (!(c >= 'a' && c <= 'z' || c == ' '))
+                        {
+                            throw new SecretMessage_Exception("");
+                        }
+                    }
+                    message = value.ToLower();
+                }
+            }
+
+            public static int CharacterConverter(char a)
+            {
+                if (a == ' ') return 26;
+                else return a - 97;
+            }
+
+            public static char IntConverter(int a)
+            {
+                if (a == 26) return ' ';
+                else return Convert.ToChar(a + 97);
+            }
+
+    }
+}
+
---
+++ src/SecretMessage/SecretMessage_Exception.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretMessage
+{
+    class SecretMessage_Exception : Exception
+    {
+        public SecretMessage_Exception(string? message) : base(message)
+        {
+        }
+    }
+}
