91e2f0913643ff4ed9f838f3a2fd5cdef573a231
tokolyidaniel <tokolyidaniel@gmail.com>
2025-05-18 15:39:23 +0200

    Created SecretDecode class with constructor and Decode method for decrypting secret messages.

---
+++ src/SecretMessage/SecretDecode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretMessage
+{
+    class SecretDecode : SecretMessage
+    {
+        public SecretDecode(string key, string secretLine) : base(key, secretLine, "unknown")
+        {
+        }
+
+        public string Decode()
+        {
+            string messageString = string.Empty;
+            for (int i = 0; i < SecretLine.Length; i++)
+            {
+                int num = CharacterConverter(SecretLine[i]) - CharacterConverter(Key[i]);
+                if (num < 0)
+                {
+                    messageString += IntConverter((27 + num));
+                }
+                else messageString += IntConverter(num);
+            }
+            return messageString;
+        }
+    }
+}
